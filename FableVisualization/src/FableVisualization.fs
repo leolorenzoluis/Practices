0667b42931d99e8e0e9e8e85ebce3d139bcc5772
Leo Lorenzo Luis <leolorenzoluis@outlook.com>
2016-12-02 01:45:17 -0500

    Added tasks. Changing LF for sh file. Fixed references

--- FableVisualization/src/FableVisualization.fs
+++ FableVisualization/src/FableVisualization.fs
@@ -2,4 +2,13 @@ open Suave                 // always open suave
 open Suave.Successful      // for OK-result
 open Suave.Web             // for config
 
+let a = 5 + 5
+let test = [1..100]
+printfn "%A" a
+
+let aa = ['a'..'z']
+
+for i in aa do
+    printfn "%A" i
+
 startWebServer defaultConfig (OK "Hello World!")
