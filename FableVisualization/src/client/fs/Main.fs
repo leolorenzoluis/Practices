cfc9c59a5d8c10b8f0672173eb94f0d818528f44
Leo Lorenzo Luis <leolorenzoluis@outlook.com>
2016-12-04 04:32:29 -0500

    Implementing simple d3

--- FableVisualization/src/client/fs/Main.fs
+++ FableVisualization/src/client/fs/Main.fs
@@ -1,55 +1,21 @@
 module App.Main
 
-
+open System.Collections.Generic
 open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import
 open Fable.Import.Browser
 
-type Complex = { r : double; i : double }
-type Color = { r : int; g : int; b : int; a : int }
+let dataset = [|1;10;20;60;40;30|]
 
-let maxIter = 512
 
-let height = 800
-let width = 1000
+let data = D3.Globals.select("body").selectAll("div").data(dataset)
 
-let minX = -2.1
-let maxX = 0.5
-let minY = -1.4
-let maxY = 1.4
-
-let iteratePoint (s : Complex) (p : Complex) : Complex =
-    { r = s.r + p.r*p.r - p.i*p.i; i = s.i + 2.0 * p.i * p.r }
-
-let getIterationCount (p : Complex) =
-    let mutable z = p
-    let mutable i = 0
-    while i < maxIter && (z.r*z.r + z.i*z.i < 4.0) do
-      z <- iteratePoint p z
-      i <- i + 1
-    i
-
-let iterCountToColor (i : int) : Color =
-    let i = maxIter - i
-    { r = 0; g = i % 256; b = 100 * (i / 256); a = 255 }
-
-let getCoordColor (x : int, y : int) : Color =
-    let p = { r = float x * (maxX - minX) / float width + minX
-            ; i = float y * (maxY - minY) / float height + minY }
-    let i = getIterationCount p
-    iterCountToColor i
- 
-let showSet() =
-    let ctx = document.getElementsByTagName_canvas().[0].getContext_2d()
-
-    let img = ctx.createImageData(U2.Case1 (float width), float height)
-    for y = 0 to height-1 do
-        for x = 0 to width-1 do
-            let index = (x + y * width) * 4
-            let color = getCoordColor (x, y)
-            img.data.[index+0] <- float color.r
-            img.data.[index+1] <- float color.g
-            img.data.[index+2] <- float color.b
-            img.data.[index+3] <- float color.a
-    ctx.putImageData(img, 0., 0.)
-
-showSet()
+D3.Globals.select "body" 
+|> fun x -> x.selectAll "div" 
+|> fun x -> x.data dataset 
+|> fun x -> (unbox<D3.Selection.Update<int>> data).enter() 
+|> fun x -> x.append "div"
+|> fun x -> x.attr("class", U3.Case2 "bar")
+|> fun x -> x.style("height", fun y _ _ -> U3.Case2 (string y+"px"))
+|> ignore
