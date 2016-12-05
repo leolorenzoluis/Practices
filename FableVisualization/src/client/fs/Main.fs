e63ec8575f974b4107482937f294de4dccff08bf
Leo Lorenzo Luis <leolorenzoluis@outlook.com>
2016-12-05 00:54:29 -0500

    Implemented scalable width for bars. If dataset is bigger, then have to update the formula

--- FableVisualization/src/client/fs/Main.fs
+++ FableVisualization/src/client/fs/Main.fs
@@ -10,23 +10,27 @@ open Fable.Import.Browser
 
 let random = new System.Random()
 
-let dataset = Array.init 15 (fun _ -> (random.Next(0,30) * 5))
+let dataset = Array.init 1000 (fun _ -> (random.Next(0,30) * 5))
 
 let barHeight x = x * 5 
 
+let barPadding = 1.
+let dataSetLength = float dataset.Length
+
 let svg = D3.Globals.select("body")
                     .append("svg")
-                    .attr("width", U3.Case1 500.)
+                    .attr("width", U3.Case1 2000.)
                     .attr("height", U3.Case1 500.)
 
 svg.selectAll("rect")
     .data(dataset)
 |> fun x -> (unbox<D3.Selection.Update<int>> x).enter()
 |> fun x -> x.append("rect")
-|> fun x -> x.attr("width", U3.Case1 20.)
+|> fun x -> x.attr("width", fun _ _ _ -> U3.Case1 (System.Math.Abs(2000. / dataSetLength - barPadding)))
                 .attr("height", U3.Case1 100.)
-                .attr("x", fun _ x y -> U3.Case1 (x * (500./(float dataset.Length)))) 
-                .attr("y", U3.Case1 100.)
+                .attr("x", fun _ x y -> printfn "X: %A Y: %A" x y
+                                        U3.Case1 (x * (2000./dataSetLength))) 
+                .attr("y", U3.Case1 100.) |> ignore
             
 
 
