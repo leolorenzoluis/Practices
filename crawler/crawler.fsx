196ff4f1c7fa82f8a68e2135e5f8fe3e8078fea3
Leo Lorenzo Luis <leolorenzoluis@outlook.com>
2016-12-07 01:34:12 -0500

    A little bit of exploration...

--- crawler/crawler.fsx
+++ crawler/crawler.fsx
@@ -4,7 +4,28 @@ open System.Globalization
 open FSharp.Data
 
 // Configure the type provider
-type DataGov = 
-  HtmlProvider<"http://google.com">
+type dataGovType = 
+  HtmlProvider<"http://data.gov.ph/catalogue/dataset?q=&sort=score+desc%2C+metadata_modified+desc&page=4">
 
-let sample = DataGov.GetSample()
+let dataGov = dataGovType.GetSample()
+
+let getLinks (htmlDocument : HtmlDocument) = 
+            htmlDocument.Descendants["a"]
+            |> Seq.choose (fun x -> x.TryGetAttribute("href")
+                                    |> Option.map(fun y -> x.InnerText(), y.Value()))
+
+
+let filteredLinks (links : seq<string * string>) = 
+            links 
+            |> Seq.filter(fun (name, link) -> link.StartsWith("/catalogue") || not (link.Contains("/catalogue/api")))
+
+let interestingLinks = getLinks(dataGov.Html) 
+                        |> filteredLinks
+
+for a in interestingLinks do
+  printfn "======= LOADING %A ========" (snd a)
+  HtmlDocument.Load("http://data.gov.ph" + (snd a))
+  |> getLinks
+  |> filteredLinks
+  |> Seq.toList
+  |> printfn "%A"
